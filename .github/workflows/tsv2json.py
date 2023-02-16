fd594b15dd1fc5aa882c1dd6337c7bc179362bf1
Rakhmanov Timur <92419757+rakhmanov-tr@users.noreply.github.com>
2023-02-16 14:44:15 +0300

    Update tsv2json.py

--- .github/workflows/tsv2json.py
+++ .github/workflows/tsv2json.py
@@ -6,7 +6,7 @@ def tsv_to_json():
   converters = [str, int, str, str, float, float, str, str, str, float, str, float, str, str, str, float, str, str, str]
   rows = [[fxn(x) for x, fxn in zip(row, converters)] for i, row in df[columns].iterrows()]
   datatables_format = {'data': rows}
-  with open('OilMetagenomesDB/crude_oil/samples/crude_oil_samples.json', 'w') as write_file:
+  with open('https://github.com/agni-bioinformatics-lab/OilMetagenomesDB/blob/main/crude_oil/samples/crude_oil_samples.json', 'w') as write_file:
     json.dump(datatables_format, write_file, indent=2)
 
 if __name__ == '__main__':
