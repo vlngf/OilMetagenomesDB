bb08ae743149f4fbb983f6a5a4ded23ade9b09b0
Rakhmanov Timur <92419757+rakhmanov-tr@users.noreply.github.com>
2023-02-23 10:17:43 +0300

    Create tsv2json.py

---
+++ crude_oil/samples/tsv2json.py
@@ -0,0 +1,16 @@
+import csv
+import json
+
+# Открываем TSV-файл для чтения
+with open('crude_oil_samples.tsv', 'r') as tsvfile:
+    reader = csv.DictReader(tsvfile, delimiter='\t')
+    # Преобразуем данные в список словарей
+    rows = [dict(row) for row in reader]
+
+# Открываем JSON-файл для записи
+with open('crude_oil_samples.json', 'w') as jsonfile:
+    # Записываем данные в формате JSON
+    json.dump(rows, jsonfile)
+
+if __name__ == '__main__':
+  tsv_to_json()
