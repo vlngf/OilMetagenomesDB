66170179657e3815a12a9508ab740323fbd5ce55
Rakhmanov Timur <92419757+rakhmanov-tr@users.noreply.github.com>
2023-02-23 09:57:40 +0300

    Update tsv2json.py

--- .github/workflows/tsv2json.py
+++ .github/workflows/tsv2json.py
@@ -1,13 +1,16 @@
-import pandas as pd
+import csv
 import json
-def tsv_to_json():
-  df = pd.read_table('https://github.com/agni-bioinformatics-lab/OilMetagenomesDB/blob/main/crude_oil/samples/crude_oil_samples.tsv')
-  columns = df.columns.values.tolist()
-  converters = [str, int, str, str, float, float, str, str, str, float, str, float, str, str, str, float, str, str, str]
-  rows = [[fxn(x) for x, fxn in zip(row, converters)] for i, row in df[columns].iterrows()]
-  datatables_format = {'data': rows}
-  with open('https://github.com/agni-bioinformatics-lab/OilMetagenomesDB/blob/main/crude_oil/samples/crude_oil_samples.json', 'w') as write_file:
-    json.dump(datatables_format, write_file, indent=2)
+
+# Открываем TSV-файл для чтения
+with open('crude_oil_samples.tsv', 'r') as tsvfile:
+    reader = csv.DictReader(tsvfile, delimiter='\t')
+    # Преобразуем данные в список словарей
+    rows = [dict(row) for row in reader]
+
+# Открываем JSON-файл для записи
+with open('crude_oil_samples.json', 'w') as jsonfile:
+    # Записываем данные в формате JSON
+    json.dump(rows, jsonfile)
 
 if __name__ == '__main__':
   tsv_to_json()
