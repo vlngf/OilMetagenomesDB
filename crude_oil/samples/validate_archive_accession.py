95e9fde34403241878ff6507037dd6cc7afc11a7
Rakhmanov Timur <92419757+rakhmanov-tr@users.noreply.github.com>
2023-03-17 21:55:27 +0300

    Create validate_archive_accession.py

---
+++ crude_oil/samples/validate_archive_accession.py
@@ -0,0 +1,46 @@
+import csv
+import json
+
+# Открываем TSV-файл для чтения
+with open('crude_oil_samples.tsv', 'r') as tsvfile:
+    reader = csv.DictReader(tsvfile, delimiter='\t')
+    # Преобразуем данные в список словарей, приводя значения полей к нужным типам данных
+    rows = [
+        {
+            "project_name": row["project_name"],
+            "publication_year": int(row["publication_year"]),
+            "publication_doi": row["publication_doi"],
+            "site_name": row["site_name"],
+            "latitude": float(row["latitude"]),
+            "longitude": float(row["longitude"]),
+            "geo_loc_name": row["geo_loc_name"],
+            "study_primary_focus": row["study_primary_focus"],
+            "sequence_name": row["sequence_name"],
+            "depth": float(row["depth"]),
+            "sample_name": row["sample_name"],
+            "sample_age": int(row["sample_age"]),
+            "sample_age_doi": row["sample_age_doi"],
+            "feature": row["feature"],
+            "material": row["material"],
+            "sampling_date": int(row["sampling_date"]),
+            "archive": row["archive"],
+            "archive_project": row["archive_project"],
+            "archive_accession": row["archive_accession"]
+        }
+        for row in reader
+    ]
+
+# Открываем JSON-файл для записи
+with open('crude_oil_samples.json', 'w') as jsonfile:
+    # Записываем данные в формате JSON
+    json.dump(rows, jsonfile, indent=4, ensure_ascii=False)
+    
+# Check uniqueness of "archive_accession" values
+archive_accession = data['archive_accession']
+if archive_accession:
+    values = set(archive_accession.split(','))
+    if len(values) != len(archive_accession):
+        print('Error: Duplicate values found in "archive_accession"')
+        exit(1)
+    else:
+      print('All archive_accession are unique')
