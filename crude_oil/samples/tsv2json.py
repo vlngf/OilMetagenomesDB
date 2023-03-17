f12549fdecf82686574fade8449a484836d0cc13
Rakhmanov Timur <92419757+rakhmanov-tr@users.noreply.github.com>
2023-03-17 11:24:00 +0300

    Update tsv2json.py

--- crude_oil/samples/tsv2json.py
+++ crude_oil/samples/tsv2json.py
@@ -1,6 +1,13 @@
 import csv
 import json
 
+# Определяем функцию для преобразования чисел в int
+def convert_ints(obj):
+    for key, value in obj.items():
+        if isinstance(value, str) and value.isdigit():
+            obj[key] = int(value)
+    return obj
+
 # Открываем TSV-файл для чтения
 with open('crude_oil_samples.tsv', 'r') as tsvfile:
     reader = csv.DictReader(tsvfile, delimiter='\t')
@@ -9,5 +16,5 @@ with open('crude_oil_samples.tsv', 'r') as tsvfile:
 
 # Открываем JSON-файл для записи
 with open('crude_oil_samples.json', 'w') as jsonfile:
-    # Записываем данные в формате JSON
-    json.dump(rows, jsonfile, indent=4, ensure_ascii=False, parse_int=int)
+    # Записываем данные в формате JSON с сохранением числовых значений как int
+    json.dump(rows, jsonfile, indent=4, ensure_ascii=False, default=convert_ints)
