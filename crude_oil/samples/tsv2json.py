d84ba58069b825e16f0a82783fb5e81c905c9a9a
Rakhmanov Timur <92419757+rakhmanov-tr@users.noreply.github.com>
2023-03-17 11:42:57 +0300

    Update tsv2json.py

--- crude_oil/samples/tsv2json.py
+++ crude_oil/samples/tsv2json.py
@@ -1,20 +1,36 @@
 import csv
 import json
 
-# Определяем функцию для преобразования чисел в int
-def convert_ints(obj):
-    for key, value in obj.items():
-        if isinstance(value, str) and value.isdigit():
-            obj[key] = int(value)
-    return obj
-
 # Открываем TSV-файл для чтения
-with open('crude_oil_samples.tsv', 'r') as tsvfile:
+with open('my_data.tsv', 'r') as tsvfile:
     reader = csv.DictReader(tsvfile, delimiter='\t')
-    # Преобразуем данные в список словарей
-    rows = [dict(row) for row in reader]
+    # Преобразуем данные в список словарей, приводя значения полей к нужным типам данных
+    rows = [
+        {
+            "project_name": row["project_name"],
+            "publication_year": int(row["publication_year"]),
+            "publication_doi": row["publication_doi"],
+            "site_name": row["site_name"],
+            "latitude": float(row["latitude"]),
+            "longitude": float(row["longitude"]),
+            "geo_loc_name": row["geo_loc_name"],
+            "study_primary_focus": row["study_primary_focus"],
+            "sequence_name": row["sequence_name"],
+            "depth": float(row["depth"]),
+            "sample_name": row["sample_name"],
+            "sample_age": int(row["sample_age"]),
+            "sample_age_doi": row["sample_age_doi"],
+            "feature": row["feature"],
+            "material": row["material"],
+            "sampling_date": row["sampling_date"],
+            "archive": row["archive"],
+            "archive_project": row["archive_project"],
+            "archive_accession": row["archive_accession"]
+        }
+        for row in reader
+    ]
 
 # Открываем JSON-файл для записи
-with open('crude_oil_samples.json', 'w') as jsonfile:
-    # Записываем данные в формате JSON с сохранением числовых значений как int
-    json.dump(rows, jsonfile, indent=4, ensure_ascii=False, default=convert_ints)
+with open('my_data.json', 'w') as jsonfile:
+    # Записываем данные в формате JSON
+    json.dump(rows, jsonfile, indent=4, ensure_ascii=False)
