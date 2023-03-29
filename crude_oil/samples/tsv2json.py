32d4a2c595a545f0e6d1237c48274948dff3d7f4
Rakhmanov Timur <92419757+rakhmanov-tr@users.noreply.github.com>
2023-03-29 12:36:41 +0300

    Update tsv2json.py

--- crude_oil/samples/tsv2json.py
+++ crude_oil/samples/tsv2json.py
@@ -7,25 +7,25 @@ with open('crude_oil_samples.tsv', 'r') as tsvfile:
     # Преобразуем данные в список словарей, приводя значения полей к нужным типам данных
     rows = [
         {
-            "project_name": row["project_name"],
+            "project_name": str(row["project_name"]),
             "publication_year": int(row["publication_year"]),
-            "publication_doi": row["publication_doi"],
-            "site_name": row["site_name"],
+            "publication_doi": str(row["publication_doi"]),
+            "site_name": str(row["site_name"]),
             "latitude": float(row["latitude"].replace(",", ".")) if isinstance(row["latitude"], str) and row["latitude"].replace(".", "", 1).isdigit() else None,
             "longitude": float(row["longitude"].replace(",", ".")) if isinstance(row["longitude"], str) and row["longitude"].replace(".", "", 1).isdigit() else None,
-            "geo_loc_name": row["geo_loc_name"],
-            "study_primary_focus": row["study_primary_focus"],
-            "sequence_name": row["sequence_name"],
+            "geo_loc_name": str(row["geo_loc_name"]),
+            "study_primary_focus": str(row["study_primary_focus"]),
+            "sequence_name": str(row["sequence_name"]),
             "depth": float(row["depth"].replace(",", ".")) if isinstance(row["depth"], str) and row["depth"].replace(".", "", 1).isdigit() else None,
-            "sample_name": row["sample_name"],
+            "sample_name": str(row["sample_name"]),
             "sample_age": int(row["sample_age"]) if row["sample_age"] != "NA" else None,
-            "sample_age_doi": row["sample_age_doi"] if row["sample_age_doi"] != "NA" else None,
-            "feature": row["feature"],
-            "material": row["material"],
+            "sample_age_doi": str(row["sample_age_doi"]) if row["sample_age_doi"] != "NA" else None,
+            "feature": str(row["feature"]),
+            "material": str(row["material"]),
             "sampling_date": int(row["sampling_date"]) if row["sampling_date"] != "NA" else None,
-            "archive": row["archive"],
-            "archive_project": row["archive_project"],
-            "archive_accession": row["archive_accession"]
+            "archive": str(row["archive"]),
+            "archive_project": str(row["archive_project"]),
+            "archive_accession": str(row["archive_accession"])
         }
         for row in reader
     ]
