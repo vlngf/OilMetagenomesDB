8e0c33d9cfcd7c80b02b51a0ed8b090c402156b0
rakhmanov-tr <tr.rakhmanov@gmail.com>
2023-06-06 13:17:34 +0500

    Add logging

--- crude_oil/libraries/validate_archive_accession.py
+++ crude_oil/libraries/validate_archive_accession.py
@@ -1,6 +1,10 @@
 import csv
 import json
-    
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+
 # Open and read the JSON file
 with open('crude_oil_libraries.json', 'r') as jsonfile:
     # Load the JSON data as a dictionary object
@@ -15,14 +19,14 @@ for index, item in enumerate(data):
         accession_list = [value.strip() for value in archive_accession.split(',') if value.strip()]
         for accession in accession_list:
             if accession in all_accessions:
-                print(f'Duplicate value {accession} found in rows {accession_dict[accession]+2} and {index+2}')
+                logging.info(f'Duplicate value {accession} found in rows {accession_dict[accession]+2} and {index+2}')
             else:
                 all_accessions.add(accession)
                 accession_dict[accession] = index
 
 # Check if the number of unique values in the set is equal to the total number of values in the "archive_sample_accession" column
 if len(all_accessions) != sum(1 for item in data if item['archive_sample_accession']):
-    print('Error: Duplicate values found in "archive_sample_accession"')
+    logging.error('Error: Duplicate values found in "archive_sample_accession"')
     exit(1)
 else:
-    print('All archive_sample_accession are unique')
+    logging.info('All archive_sample_accession are unique')
--- crude_oil/samples/validate_gaps.py
+++ crude_oil/samples/validate_gaps.py
@@ -1,18 +1,22 @@
 import pandas as pd
 import numpy as np
+import logging
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
 
 # Загрузка файла данных
 data = pd.read_csv('crude_oil_samples.tsv', delimiter='\t')
 # unique использовал, чтобы узнать какие значения воспринимает код (None, nan (для NA и пустых строк))
-# print(data.iloc[:, 2].unique())
+# logging.info(data.iloc[:, 2].unique())
 # Проверка на наличие пустых ячеек (исключая ячейки со значением 'None')
 empty_cells = np.where(pd.isna(data))
 
 # Вывод номеров строк и колонок с пустыми ячейками
 if len(empty_cells[0]) > 0:
-    print('Пустые ячейки найдены в следующих местах:')
+    logging.error('Пустые ячейки найдены в следующих местах:')
     for row, col in zip(empty_cells[0], empty_cells[1]):
-        print(f"Строка {row}, колонка {col}")
+        logging.error(f"Строка {row}, колонка {col}")
     exit(1)
 else:
-    print('Пустых ячеек не найдено')
+    logging.info('Пустых ячеек не найдено')
