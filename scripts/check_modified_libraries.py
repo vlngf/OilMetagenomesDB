70baa7bde7f2e90bcaae6d86ddcdba06d77d073c
rakhmanov-tr <tr.rakhmanov@gmail.com>
2023-06-09 17:29:19 +0500

    update check_modified scripts

--- scripts/check_modified_libraries.py
+++ scripts/check_modified_libraries.py
@@ -12,8 +12,11 @@ subprocess.run(["git", "checkout", "FETCH_HEAD", "--", os.environ["FILE_PATH"]])
 # Load the old file
 df_fork = pd.read_csv(os.environ["FILE_PATH"], sep="\t")
 
+# Merge the two DataFrames and create a new column '_merge' indicating where each row is from
+merged = df_fork.merge(df_pr, how='outer', indicator=True)
+
 # Check if any of the old rows have been modified or deleted
-if not df_fork.isin(df_pr).all().all():
+if (merged['_merge'] == 'left_only').any():
     print("Old rows in the file have been modified or deleted")
     exit(1)
 
