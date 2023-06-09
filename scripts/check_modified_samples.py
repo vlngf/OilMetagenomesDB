16b88c66452805706f0824084366847cd72f2eca
rakhmanov-tr <tr.rakhmanov@gmail.com>
2023-06-09 17:19:08 +0500

    update check_modified scripts

--- scripts/check_modified_samples.py
+++ scripts/check_modified_samples.py
@@ -1,22 +1,22 @@
 import pandas as pd
 import os
+import subprocess
 
-# Load the tsv files
-file_path = os.environ.get('FILE_PATH')
-df_PR = pd.read_csv(file_path, sep='\t')
+# Load the file from the pull request
+df_pr = pd.read_csv(os.environ["FILE_PATH"], sep="\t")
 
-# Replace 'main' with 'master' if your default branch is master
-df_fork = pd.read_csv(f"https://raw.githubusercontent.com/{os.environ.get('GITHUB_REPOSITORY')}/main/common_samples/common_samples.tsv", sep='\t')
+# Fetch the version of the file when the fork was created
+subprocess.run(["git", "fetch", "origin", "main"])
+subprocess.run(["git", "checkout", "FETCH_HEAD", "--", os.environ["FILE_PATH"]])
 
-# Check if any rows have been deleted
-if len(df_PR) < len(df_fork):
-    print("Rows have been deleted from the TSV file. This is not allowed.")
+# Load the old file
+df_fork = pd.read_csv(os.environ["FILE_PATH"], sep="\t")
+
+# Check if any of the old rows have been modified or deleted
+common = df_pr.merge(df_fork, how='inner')
+if not common.equals(df_fork):
+    print("Old rows in the file have been modified or deleted")
     exit(1)
 
-# Check if any existing values have been modified
-for column in df_PR.columns:
-    if any(df_PR[column] != df_fork[column]):
-        modified_rows = df_PR.index[df_PR[column] != df_fork[column]].tolist()
-        for row in modified_rows:
-            print(f"Value in column {column} and row {row} has been modified. This is not allowed.")
-        exit(1)
+# Everything is OK
+print("No old rows have been modified or deleted")
