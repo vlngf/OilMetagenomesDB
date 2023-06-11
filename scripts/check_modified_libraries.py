2d3dfab38b1ca018cf0aa1fd0a0bfb88133c1b36
rakhmanov-tr <tr.rakhmanov@gmail.com>
2023-06-11 15:15:39 +0500

    update scripts and yml

--- scripts/check_modified_libraries.py
+++ scripts/check_modified_libraries.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import os
 import subprocess
+import json
+from jsonschema import validate, ValidationError
+import sys
 
 # Load the file from the pull request
 df_pr = pd.read_csv(os.environ["FILE_PATH"], sep="\t")
@@ -20,9 +23,26 @@ if (merged['_merge'] == 'left_only').any():
     print("\033[31mOld rows in common_libraries.tsv have been modified or deleted\033[0m")
     exit(1)
 
-command = 'echo -n "\033[38;5;40mNo old rows have been modified or deleted in common_samples.tsv\033[0m"'
+command = 'echo -n "\033[38;5;40mNo old rows have been modified or deleted in common_libraries.tsv\033[0m"'
 subprocess.call(command, shell=True)
 
 new_rows = df_pr[~df_pr.isin(df_fork)].dropna()
-print("new_rows")
-print(new_rows)
+
+# Загрузка схемы JSON
+with open("assets/commons/common_libraries.json", "r") as file:
+    schema = json.load(file)
+
+# Валидация каждой новой строки
+errors_found = False  # Флаг для отслеживания наличия ошибок
+
+for idx, row in new_rows.iterrows():
+    try:
+        validate(instance=row.to_dict(), schema=schema)
+        # print(f"Валидация прошла успешно для строки {idx}")
+    except ValidationError as e:
+        errors_found = True
+        print(f"Ошибка в строке {idx}, колонка '{e.path[0]}', значение '{row[e.path[0]]}': {e.message}")
+
+# Проверка флага наличия ошибок
+if errors_found:
+    sys.exit(1)
