5a582859bad23057155f385003a8ceedc1b25bee
rakhmanov-tr <tr.rakhmanov@gmail.com>
2023-06-11 16:35:10 +0500

    update scripts

--- scripts/check_modified_libraries.py
+++ scripts/check_modified_libraries.py
@@ -15,11 +15,11 @@ subprocess.run(["git", "checkout", "FETCH_HEAD", "--", os.environ["FILE_PATH"]])
 # Load the old file
 df_fork = pd.read_csv(os.environ["FILE_PATH"], sep="\t")
 
-# Merge the two DataFrames and create a new column '_merge' indicating where each row is from
-merged = df_fork.merge(df_pr, how='outer', indicator=True)
+# Concatenate the two DataFrames and create a new column '_merge' indicating where each row is from
+merged = pd.concat([df_fork, df_pr], ignore_index=True, sort=False, keys=["old", "new"])
 
 # Check if any of the old rows have been modified or deleted
-if (merged['_merge'] == 'left_only').any():
+if (merged["_merge"] == "left_only").any():
     print("\033[31mOld rows in common_libraries.tsv have been modified or deleted\033[0m")
     exit(1)
 
