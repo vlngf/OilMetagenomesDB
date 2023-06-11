761d7376684b594cc3f618490b8baf1a5748d4a6
rakhmanov-tr <tr.rakhmanov@gmail.com>
2023-06-11 17:14:41 +0500

    update scripts

--- scripts/check_modified_libraries.py
+++ scripts/check_modified_libraries.py
@@ -20,6 +20,11 @@ new_rows = df_pr[~df_pr.isin(df_fork)].dropna()
 # Remove new_rows from df_pr
 df_pr = df_pr.drop(new_rows.index)
 
+print('df_fork')
+print(df_fork)
+print('df_pr')
+print(df_pr)
+
 # Check if df_fork and df_pr are identical
 if not df_fork.equals(df_pr):
     print("\033[31mOld rows in common_samples.tsv have been modified or deleted\033[0m")
