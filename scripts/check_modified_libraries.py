7f327dc779a9ffb21583749c764966140df9a15b
rakhmanov-tr <tr.rakhmanov@gmail.com>
2023-06-11 18:10:04 +0500

    update scripts

--- scripts/check_modified_libraries.py
+++ scripts/check_modified_libraries.py
@@ -17,19 +17,20 @@ df_fork = pd.read_csv(os.environ["FILE_PATH"], sep="\t")
 
 new_rows = df_pr[~df_pr.isin(df_fork)].dropna()
 
+df_fork_check = df_fork
 # Drop new_rows from df_pr and save it to df_pr_check
 df_pr_check = df_pr.drop(new_rows.index)
 
-print('df_fork')
-print(df_fork)
+print('df_fork_check')
+print(df_fork_check)
 print('df_pr_check')
 print(df_pr_check)
 
-df_fork.reset_index(drop=True, inplace=True)
+df_fork_check.reset_index(drop=True, inplace=True)
 df_pr_check.reset_index(drop=True, inplace=True)
 
 # Check if df_fork and df_pr are identical
-if not df_fork.equals(df_pr_check):
+if not df_fork_check.equals(df_pr_check):
     print("\033[31mOld rows in common_samples.tsv have been modified or deleted\033[0m")
     exit(1)
 
