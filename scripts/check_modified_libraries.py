104175a7d6d73e684de9e0884a4aab74e910433b
rakhmanov-tr <tr.rakhmanov@gmail.com>
2023-08-07 01:00:31 +0300

    update check_.yml

--- scripts/check_modified_libraries.py
+++ scripts/check_modified_libraries.py
@@ -15,11 +15,6 @@ subprocess.run(["git", "checkout", "FETCH_HEAD", "--", os.environ["FILE_PATH"]])
 # Load the old file
 df_fork = pd.read_csv(os.environ["FILE_PATH"], sep="\t")
 
-# Check string uniqueness in df_fork
-if df_fork.duplicated().any():
-    print("Rows are not unique in common_libraries.tsv")
-    sys.exit(1)
-
 new_rows = df_pr.loc[df_pr.index[len(df_fork):]]
 
 df_fork_check = df_fork.copy()
@@ -27,56 +22,20 @@ df_fork_check = df_fork.copy()
 # Drop new_rows from df_pr and save it to df_pr_check
 df_pr_check = df_pr.drop(new_rows.index)
 
-print('df_fork_check')
+# Вывод содержимого таблиц
+print('\ndf_fork_check')
 print(df_fork_check)
-print('df_pr_check')
+print()
+print('\ndf_pr_check')
 print(df_pr_check)
+print()
 
-df_fork_check.reset_index(drop=True, inplace=True)
-df_pr_check.reset_index(drop=True, inplace=True)
-
-# Normalize the data in df_fork_check and df_pr_check
-df_fork_check = df_fork_check.applymap(lambda x: x.strip() if isinstance(x, str) else x)
-df_fork_check = df_fork_check.applymap(lambda x: x.lower() if isinstance(x, str) else x)
-
-df_pr_check = df_pr_check.applymap(lambda x: x.strip() if isinstance(x, str) else x)
-df_pr_check = df_pr_check.applymap(lambda x: x.lower() if isinstance(x, str) else x)
+# Вывод содержимого new_rows
+print("\nСодержимое new_rows:")
+print(new_rows)
 
 # Save df_pr_check to a CSV file
 df_pr_check.to_csv("df_pr_check.csv", index=False)
 
 # Save df_fork_check to a CSV file
 df_fork_check.to_csv("df_fork_check.csv", index=False)
-    
-command = 'echo -n "\033[38;5;40mNo old rows have been modified or deleted in common_libraries.tsv\033[0m"'
-subprocess.call(command, shell=True)
-
-# Вывод содержимого new_rows
-print("\nСодержимое new_rows:")
-print(new_rows)
-
-schema_file = "assets/commons/common_libraries.json"
-
-# Проверка существования файла схемы JSON
-if not os.path.isfile(schema_file):
-    print(f"Файл схемы JSON '{schema_file}' не найден.")
-    sys.exit(1)
-
-# Загрузка схемы JSON
-with open("assets/commons/common_libraries.json", "r") as file:
-    schema = json.load(file)
-
-# Валидация каждой новой строки
-errors_found = False  # Флаг для отслеживания наличия ошибок
-
-for idx, row in new_rows.iterrows():
-    try:
-        validate(instance=row.to_dict(), schema=schema)
-        print(f"Валидация прошла успешно для строки {idx}")
-    except ValidationError as e:
-        errors_found = True
-        print(f"Ошибка в строке {idx}: {e.message}")
-
-# Проверка флага наличия ошибок
-if errors_found:
-    sys.exit(1)
