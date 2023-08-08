9a69f12673932c5b8ab186dab7459a3d76b3c803
rakhmanov-tr <tr.rakhmanov@gmail.com>
2023-08-08 17:40:34 +0300

    update scripts

--- scripts/validation_libraries.py
+++ scripts/validation_libraries.py
@@ -5,44 +5,34 @@ import json
 from jsonschema import validate, ValidationError
 import sys
 
-print("it's working")
-
-# Load the file from the pull request
+# Read the common_libraries.tsv from the pull request into a DataFrame
 df_pr = pd.read_csv(os.environ["LIBRARIES_PATH"], sep="\t")
 
-# Fetch the version of the file when the fork was created
+# Fetch the main branch from the repository
 subprocess.run(["git", "fetch", "origin", "main"])
+
+# Checkout the common_libraries.tsv from the main branch
 subprocess.run(["git", "checkout", "FETCH_HEAD", "--", os.environ["LIBRARIES_PATH"]])
 
-# Load the old file
+# Read the common_libraries.tsv from the main branch into a DataFrame
 df_fork = pd.read_csv(os.environ["LIBRARIES_PATH"], sep="\t")
 
+# Extract the new rows that have been added in the pull request
 new_rows = df_pr.loc[df_pr.index[len(df_fork):]]
 
+# Create a copy of the df_fork and df_pr for checking
 df_fork_check = df_fork.copy()
-
-# Drop new_rows from df_pr and save it to df_pr_check
 df_pr_check = df_pr.drop(new_rows.index)
 
-# Вывод содержимого таблиц
-print('\ndf_fork_check')
-print(df_fork_check)
-print()
-print('\ndf_pr_check')
-print(df_pr_check)
-print()
-
-# Вывод содержимого new_rows
-print("\nСодержимое new_rows:")
+# Print the content of the new rows added in the pull request
+print("\nContent of new_rows:")
 print(new_rows)
 
-# Сравнение фреймов данных
+# Compare the DataFrames of the pull request and the main branch
 comparison_result = df_pr_check.compare(df_fork_check)
-
-# Вывод различий
 if comparison_result.empty:
-    print("CSV files are identical, let's continue validation")
+    print("\033[38;5;40mThe old rows haven't been changed\033[0m")
 else:
-    print("Differences between CSV files:")
+    print("\033[31mThe old rows have been changed:\033[0m")
     print(comparison_result)
     sys.exit(1)
