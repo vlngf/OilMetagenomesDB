a13c26587725af8c37d44b88a931567d3169d3f8
rakhmanov-tr <tr.rakhmanov@gmail.com>
2023-08-09 11:09:08 +0300

    update scripts

--- scripts/validation_libraries.py
+++ scripts/validation_libraries.py
@@ -2,8 +2,9 @@ import pandas as pd
 import os
 import subprocess
 import json
-from jsonschema import validate, ValidationError
+from jsonschema import ValidationError
 import sys
+import re 
 
 # Read the common_libraries.tsv from the pull request into a DataFrame
 df_pr = pd.read_csv(os.environ["LIBRARIES_PATH"], sep="\t")
@@ -35,4 +36,58 @@ if comparison_result.empty:
 else:
     print("\033[31mThe old rows have been changed:\033[0m")
     print(comparison_result)
+    sys.exit(1)
+
+# Выше все работает правильно 
+
+# Функция валидации
+def is_valid(row, schema):
+
+    # Проверка типа
+    if 'type' in schema:
+        if schema['type'] == 'string' and not isinstance(row, str):
+            print(f"Ошибка в колонке {column} в строке '{row}' -> неверный тип данных. Ожидаемый тип: {schema['type']}")
+            return False
+        elif schema['type'] == 'integer' and not isinstance(row, int):
+            print(f"Ошибка в колонке {column} в строке '{row}' -> неверный тип данных. Ожидаемый тип: {schema['type']}")
+            return False
+
+    # Проверка по паттерну
+    if 'pattern' in schema and not re.match(schema['pattern'], str(row)):
+        print(f"Ошибка в колонке {column} в строке '{row}' -> не соответствует ожидаемому паттерну: {schema['pattern']}")
+        return False
+
+    # Проверка по списку возможных значений
+    if 'enum' in schema and row not in schema['enum']:
+        print(f"Ошибка в колонке {column} в строке '{row}' -> значение не из списка возможных: {schema['enum']}")
+        return False
+
+    return True
+
+columns_to_remove = ["publication_year", "library_concentration", "PCR_cycle_count", "read_count", "download_sizes"]
+columns = new_rows.columns.drop(columns_to_remove)
+new_rows = new_rows.where((pd.notna(new_rows)), None)
+
+# Путь к папке с JSON-файлами в репозитории
+schemas_path = os.path.join(os.environ["GITHUB_WORKSPACE"], 'schemas_libraries')
+error_flag = False
+
+# Итерация по списку колонок
+for column in columns:
+    # Поиск соответствующего JSON файла
+    json_file = [file for file in os.listdir(schemas_path) if file.endswith('.json') and file.startswith(column)]
+
+    json_file_path = os.path.join(schemas_path, json_file[0])
+    with open(json_file_path, 'r') as file:
+        schema = json.load(file)
+
+    # Применяем функцию валидации к соответствующему столбцу
+    validation_results = new_rows[column].apply(is_valid, schema=schema['properties'][column])
+    if not all(validation_results):
+        error_flag = True
+    else:
+        print('Успешная валидация колонки:', column)
+    print()
+
+if error_flag:
     sys.exit(1)
