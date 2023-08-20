5c96b31631220ab880f5246d66320fa4d7aaca16
rakhmanov-tr <tr.rakhmanov@gmail.com>
2023-08-20 15:08:37 +0300

    update scripts

--- scripts/validation_libraries.py
+++ scripts/validation_libraries.py
@@ -2,80 +2,99 @@ import pandas as pd
 import numpy as np
 import os, sys, re, json, subprocess
 from jsonschema import validate, ValidationError
+import logging
 
-# Read the common_libraries.tsv from the pull request into a DataFrame to compare
-df_pr = pd.read_csv(os.environ["LIBRARIES_PATH"], sep="\t")
+# Настройка логгирования
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Read the common_libraries.tsv from the pull request into a DataFrame to validate
-# Параметр keep_default_na указывает pandas не использовать список стандартных значений для преобразования в NaN
-df_pr_ = pd.read_csv(os.environ["LIBRARIES_PATH"], sep="\t", 
-                     dtype={"publication_year": str, 
-                            "strand_type": str,
-                            "library_polymerase": str,
-                            "library_concentration": str,
-                            "library_treatment": str,
-                            "PCR_cycle_count": str,
-                            "read_count": str,
-                            "download_sizes": str},
-                            keep_default_na=False)
 
-# Fetch the main branch from the repository
-subprocess.run(["git", "fetch", "origin", "main"])
+def read_dataframe_from_path(path):
+    return pd.read_csv(path, sep="\t")
 
-# Checkout the common_libraries.tsv from the main branch
-subprocess.run(["git", "checkout", "FETCH_HEAD", "--", os.environ["LIBRARIES_PATH"]])
 
-# Read the common_libraries.tsv from the main branch into a DataFrame
-df_fork = pd.read_csv(os.environ["LIBRARIES_PATH"], sep="\t")
+def read_dataframe_for_validation(path):
+    return pd.read_csv(path, sep="\t",
+                       dtype={
+                           "publication_year": str,
+                           "strand_type": str,
+                           "library_polymerase": str,
+                           "library_concentration": str,
+                           "library_treatment": str,
+                           "PCR_cycle_count": str,
+                           "read_count": str,
+                           "download_sizes": str
+                       },
+                       keep_default_na=False)
 
-# Create a copy of the df_fork and df_pr for checking
-df_fork_check = df_fork.copy()
-df_pr_check = df_pr.drop(df_pr.loc[df_pr.index[len(df_fork):]].index)
 
-# Compare the DataFrames of the pull request and the main branch
-comparison_result = df_pr_check.compare(df_fork_check)
-if comparison_result.empty:
-    print("\033[38;5;40mThe old rows haven't been changed, now let's validate new rows\033[0m")
-else:
-    print("\033[31mThe old rows have been changed:\033[0m")
-    print(comparison_result)
-    sys.exit(1)
+def fetch_and_checkout_branch(branch_name, path):
+    subprocess.run(["git", "fetch", "origin", branch_name])
+    subprocess.run(["git", "checkout", "FETCH_HEAD", "--", path])
 
-# Extract the new rows that have been added in the pull request
-new_rows = df_pr_.loc[df_pr_.index[len(df_fork):]]
 
-# Print the content of the new rows added in the pull request
-print("\nContent of new_rows:")
-print(new_rows)
-print()
+def compare_dataframes(df1, df2):
+    df1_check = df1.drop(df1.loc[df1.index[len(df2):]].index)
+    comparison_result = df1_check.compare(df2)
+    return comparison_result
 
-schemas_path = os.path.join(os.environ["GITHUB_WORKSPACE"], 'schemas_libraries')
-columns = new_rows.columns
-validation_results = {}
-error_value = False
 
-for column in columns:
-    json_file = [file for file in os.listdir(schemas_path) if file.endswith('.json') and os.path.splitext(file)[0] == column]
-    json_file_path = os.path.join(schemas_path, json_file[0])
-    with open(json_file_path, 'r') as file:
-        schema = json.load(file)
-    column_results = []
-    for index, value in enumerate(new_rows[column], start=df_fork.shape[0]):
-        try:
-            validate(instance=value, schema=schema)
-            column_results.append(f"Valid (Row {index})")
-        except ValidationError as e:
-            # из-за экранирования необходима замена '\\\\' на '\\'
-            error_message = e.message.replace('\\\\', '\\')
-            column_results.append(f"Invalid (Row {index}): {error_message}")
-            error_value = True
-    validation_results[column] = column_results
+def validate_new_rows(new_rows, schemas_path):
+    columns = new_rows.columns
+    validation_results = {}
+    error_value = False
 
-formatted_output = json.dumps(validation_results, ensure_ascii=False, indent=1)
-print(formatted_output)
+    for column in columns:
+        json_file = [file for file in os.listdir(schemas_path) if file.endswith('.json') and os.path.splitext(file)[0] == column]
+        json_file_path = os.path.join(schemas_path, json_file[0])
+        with open(json_file_path, 'r') as file:
+            schema = json.load(file)
+        column_results = []
+        for index, value in enumerate(new_rows[column], start=df_fork.shape[0]):
+            try:
+                validate(instance=value, schema=schema)
+                column_results.append(f"Valid (Row {index})")
+            except ValidationError as e:
+                error_message = e.message.replace('\\\\', '\\')
+                column_results.append(f"Invalid (Row {index}): {error_message}")
+                error_value = True
+        validation_results[column] = column_results
 
-if error_value:
-    print("\033[31mInvalid values found\033[0m")
-    exit(1)
-else:
-    print("\033[38;5;40mNo invalid values found\033[0m")
+    return validation_results, error_value
+
+
+def main():
+    LIBRARIES_PATH = os.environ["LIBRARIES_PATH"]
+    df_pr = read_dataframe_from_path(LIBRARIES_PATH)
+    df_pr_ = read_dataframe_for_validation(LIBRARIES_PATH)
+
+    fetch_and_checkout_branch("main", LIBRARIES_PATH)
+    df_fork = read_dataframe_from_path(LIBRARIES_PATH)
+
+    comparison_result = compare_dataframes(df_pr, df_fork)
+
+    if comparison_result.empty:
+        logging.info("The old rows haven't been changed, now let's validate new rows")
+    else:
+        logging.error("The old rows have been changed:")
+        logging.error(comparison_result)
+        sys.exit(1)
+
+    new_rows = df_pr_.loc[df_pr_.index[len(df_fork):]]
+    logging.info("\nContent of new_rows:")
+    logging.info(new_rows)
+
+    schemas_path = os.path.join(os.environ["GITHUB_WORKSPACE"], 'schemas_libraries')
+    validation_results, error_value = validate_new_rows(new_rows, schemas_path)
+
+    formatted_output = json.dumps(validation_results, ensure_ascii=False, indent=1)
+    logging.info(formatted_output)
+
+    if error_value:
+        logging.error("Invalid values found")
+        exit(1)
+    else:
+        logging.info("No invalid values found")
+
+
+if __name__ == "__main__":
+    main()
