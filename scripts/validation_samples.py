b73482a54275f0b1bdbbc24bb3b72aea02c30165
Rakhmanov Timur <92419757+rakhmanov-tr@users.noreply.github.com>
2023-09-02 19:17:38 +0500

    Update validation_samples.py

--- scripts/validation_samples.py
+++ scripts/validation_samples.py
@@ -28,8 +28,8 @@ def find_duplicate_rows(df):
     duplicated_rows = df[df.duplicated(keep=False)]
     duplicate_index_groups = defaultdict(list)
     for index, row in duplicated_rows.iterrows():
-        duplicate_index_groups[tuple(row)].append(index + 1)
-    return duplicate_index_groups
+        duplicate_index_groups[tuple(row)].append(index)
+    return {k: v for k, v in duplicate_index_groups.items() if len(v) > 1}
 
 # Check the uniqueness of specified columns in the DataFrame
 def check_column_uniqueness(df, columns):
