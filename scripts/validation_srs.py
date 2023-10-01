7593092dbc5cddc0d83a6005ae4c7ae569d6c382
Rakhmanov Timur <tr.rakhmanov@gmail.com>
2023-10-01 16:25:42 +0300

    Update SRS validation files

--- scripts/validation_srs.py
+++ scripts/validation_srs.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import os
+import os, sys
 
 # Read the DataFrame used for comparison purposes
 def read_dataframe_for_compare(path):
@@ -9,28 +9,42 @@ def read_dataframe_for_compare(path):
 def check_srs(samples, libraries):
     set_s = set(samples)
     set_l = set(libraries)
+    error_messages = []
     
     # Проверяем, что все значения из libraries присутствуют в samples
-    if not set_l.issubset(set_l):
-        return False
+    missing_from_samples = set_l - set_s
+    for index, value in enumerate(libraries):
+        if value in missing_from_samples and f"Invalid SRS of libraries (Row {index+2}): {value} is not in samples" not in error_messages:
+            error_messages.append(f"Invalid SRS of libraries (Row {index+2}): {value} is not in samples")
     
     # Проверяем, что для каждого значения из samples есть хотя бы одно соответствующее значение в libraries
-    for s in set_s:
-        if s not in libraries:
-            return False
-    return True
+    missing_from_libraries = set_s - set_l
+    for index, s in enumerate(samples):
+        if s in missing_from_libraries and f"Invalid SRS of samples (Row {index+2}): {s} is not in libraries" not in error_messages:
+            error_messages.append(f"Invalid SRS of samples (Row {index+2}): {s} is not in libraries")
+    
+    if not error_messages:
+        return True, "\033[38;5;40mAll SRS values are valid\033[0m"
+    else:
+        return False, "\n".join(error_messages)
 
 # Main execution
 def main():
     # Read the SRS column to validation
     df_s = read_dataframe_for_compare(os.environ["SAMPLES_PATH"])
     df_l = read_dataframe_for_compare(os.environ["LIBRARIES_PATH"])
+    valid, message = check_srs(df_s['archive_accession'], df_l['archive_accession'])
     
-    print(check_srs(df_s['archive_accession'], df_l['archive_accession']))
-    print('df_s')
-    print(df_s.shape)
-    print('df_l')
-    print(df_l.shape)
-    
+    # Print the validation results
+    print(message)
+
+    error_value = not valid
+    if error_value:
+        print("\033[31mInvalid values of SRS found\033[0m")
+        sys.exit(1)
+    else:
+        print("\033[38;5;40mNo invalid values of SRS found\033[0m")
+
+
 if __name__ == "__main__":
     main()
